905c4a1cbc58c5107c253a05f36c90184d2e5780
Saurav Dutta <saurav@piktorlabs.com>
2021-05-26 15:46:41 +0530

    changes for song list

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ const PORT = process.env.PORT || 8000;
  */
 let users = [];
 const messages = {
-  edm: [],
-  hiphop: [],
-  bollywood: [],
-  developer: []
+  assamese: [],
+  overseas: [],
+  hits: [],
+  missouts: []
 };
 
 const votingData = {
-  edm: [
+  assamese: [
     {
       songName: 'Motoloya 2.0',
       votingCount: 0,
@@ -33,75 +33,112 @@ const votingData = {
       current: false
     },
     {
-      songName: 'alakananda',
+      songName: 'Alakananda',
       votingCount: 0,
       url: 'https://soundcloud.com/xobdoofficial/alakananda-xobdo-remix',
       current: false
     },
     {
-      songName: 'xuronjona',
+      songName: 'Mon Mur',
+      votingCount: 0,
+      url: 'https://soundcloud.com/assamese-songs/mon-mur',
+      current: false
+    },
+    {
+      songName: 'Xuronjona',
       votingCount: 0,
       url: 'https://soundcloud.com/muzammilaslamhere/tonmoy-krypton-x-sannidhya-1',
+      current: false
+    },
+    {
+      songName: 'Jonake bisare ki',
+      votingCount: 0,
+      url: 'https://soundcloud.com/assamese-songs/jonake-bisare-ki',
       current: false
     }
   ],
-  hiphop: [
+  overseas: [
     {
-      songName: 'Motoloya 2.0',
+      songName: 'Avicii - the nights',
       votingCount: 0,
-      url: 'https://soundcloud.com/sannidhyabhuyan-music/motoliya-2-0',
+      url: 'https://soundcloud.com/adolfo-ayala-alen/avicii-the-nights',
       current: false
     },
     {
-      songName: 'alakananda',
+      songName: 'Skillet - Hero',
       votingCount: 0,
-      url: 'https://soundcloud.com/xobdoofficial/alakananda-xobdo-remix',
+      url: 'https://soundcloud.com/s-rgiokishino/skillet-hero-awake-remix',
       current: false
     },
     {
-      songName: 'xuronjona',
+      songName: 'Backstreet Boys - I want it that way',
       votingCount: 0,
-      url: 'https://soundcloud.com/muzammilaslamhere/tonmoy-krypton-x-sannidhya-1',
+      url: 'https://soundcloud.com/clemenswenners/i-want-it-that-way',
+      current: false
+    },
+    {
+      songName: 'Shape of you',
+      votingCount: 0,
+      url: 'https://soundcloud.com/jamescarterpresents/shape-of-you',
+      current: false
+    },
+    {
+      songName: 'Let Her Go',
+      votingCount: 0,
+      url: 'https://soundcloud.com/nettwerkmusicgroup/passenger-let-her-go',
       current: false
     }
   ],
-  bollywood: [
+  hits: [
     {
-      songName: 'Motoloya 2.0',
+      songName: 'Tere bin nahi lagda',
       votingCount: 0,
-      url: 'https://soundcloud.com/sannidhyabhuyan-music/motoliya-2-0',
+      url: 'https://soundcloud.com/user-836130715/tere-bin-nahi-lagda-dil-mera-dholna-by-papon',
       current: false
     },
     {
-      songName: 'alakananda',
+      songName: 'Samajavaragamana',
       votingCount: 0,
-      url: 'https://soundcloud.com/xobdoofficial/alakananda-xobdo-remix',
+      url: 'https://soundcloud.com/user-713826249/01-samajavaragamana',
       current: false
     },
     {
-      songName: 'xuronjona',
+      songName: 'Makhna',
       votingCount: 0,
-      url: 'https://soundcloud.com/muzammilaslamhere/tonmoy-krypton-x-sannidhya-1',
+      url: 'https://soundcloud.com/sabtain-amjad/makhna-drive-sushant-singh',
+      current: false
+    },
+    {
+      songName: 'Khairiyat',
+      votingCount: 0,
+      url: 'https://soundcloud.com/desimelodies/khairiyat-arijit-singh-1',
+      current: false
+    },
+    {
+      songName: 'Dheera Dheera',
+      votingCount: 0,
+      url: 'https://soundcloud.com/filmy-songs/dheera-dheera-anaya-bhatt-kgf-telugu-filmysongs',
       current: false
     }
   ],
-  developer: [
+  missouts: [
     {
-      songName: 'Motoloya 2.0',
+      songName: "Lokada Kalaji - Raghu Dixit",
       votingCount: 0,
-      url: 'https://soundcloud.com/sannidhyabhuyan-music/motoliya-2-0',
+      url: "https://soundcloud.com/trdp/raghu-dixit-jagchanga-lokada?in=trdp/sets/jag-changa",
       current: false
     },
     {
-      songName: 'alakananda',
+      songName: "making all the things new",
       votingCount: 0,
-      url: 'https://soundcloud.com/xobdoofficial/alakananda-xobdo-remix',
+      url: "https://soundcloud.com/dougwaterman/making-all-things-new-waterman?in=user-961145082/sets/as-beautiful-as-it-sounds",
       current: false
     },
     {
-      songName: 'xuronjona',
+      songName: "Stay Alive",
       votingCount: 0,
-      url: 'https://soundcloud.com/muzammilaslamhere/tonmoy-krypton-x-sannidhya-1',
+      url:
+        "https://soundcloud.com/eddy-thevenet/stay-alive-jose-gonzalez-vs",
       current: false
     }
   ]
